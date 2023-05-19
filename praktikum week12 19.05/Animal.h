4b658eb3f62dc24ce94592f536904fefedd32e6e
Valyo0430 <72649987+zzvalkatazz@users.noreply.github.com>
2023-05-19 17:06:35 +0300

    Add files via upload

---
+++ praktikum week12 19.05/Animal.h
@@ -0,0 +1,28 @@
+﻿#ifndef ANIMAL_
+#define ANIMAL_
+enum Food = { meat,noMeat,both };
+class Animal
+{
+private:
+	char* name;
+	ínt weight;
+	Food food;
+public:
+	int eat();
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
